46a2e7b1d57b9a21ab3d89651af64d97bcf0fd29
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2024-04-20 15:21:30 -0300

    Add conect to data base

--- mais-consultas-api/Program.cs
+++ mais-consultas-api/Program.cs
@@ -1,3 +1,6 @@
+using mais_consultas_api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
-var app = builder.Build();
+string? connectionString = builder.Configuration.GetConnectionString("MySql");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySql(connectionString,
+    ServerVersion.Parse("8.0.28-mysql")
+));
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(c =>
+    c.WithOrigins("*")
+        .WithMethods("PUT", "DELETE", "GET", "POST")
+        .AllowAnyHeader()
+);
 
 app.UseHttpsRedirection();
 
