b1ae9686ad58a05076303641c693ffb7d2347024
arthur.paula <arthur.paula@autoglass.com.br>
2024-04-29 19:56:50 -0300

    Cria modelo para a entidade Professional

---
+++ mais-consultas-api/Models/Professional.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Professional
+{
+	public int Id { get; protected set; }
+	public string Name { get; protected set; }
+	public Service Service { get; protected set; }
+	public Provider Provider { get; protected set; }
+
+	public Professional(string name, Service service, Provider provider)
+	{
+
+	}
+
+	public void SetName(string name) 
+	{
+		if (name.len)
+	}
+}
