989ebc434ff46745af18ee25079a83a7479181e6
Eric Andrade Eller <111186310+EricEller22@users.noreply.github.com>
2024-05-01 10:23:13 -0300

    Eric (#8)
    * Cria construtor
    
    * Classe Patient
    
    * Cria classe Patient
    
    * Criar classe Patient
    
    * Criar classe patient 3
    
    ---------
    
    Co-authored-by: Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>

---
+++ mais-consultas-api/Models/Patient.cs
@@ -0,0 +1,45 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mais_consultas_api.Models
+{
+    public class Patient
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        public string Cpf { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime BirthdayDate { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        //Constructor
+        public Patient()
+        {
+        }
+
+
+    }
+}
