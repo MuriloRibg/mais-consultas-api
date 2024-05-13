da38fb11950b0ced112fa2b96f3ee0ea393d2d4e
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2024-05-13 12:18:10 -0300

    Muda tipo da data.

--- mais-consultas-api/Models/Patient.cs
+++ mais-consultas-api/Models/Patient.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mais_consultas_api.Models
 {
@@ -21,6 +22,7 @@ namespace mais_consultas_api.Models
         public string PhoneNumber { get; set; }
 
         [Required]
+        [Column(TypeName = "date")]
         public DateTime BirthdayDate { get; set; }
 
         [Required]
