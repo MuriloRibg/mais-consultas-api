4d314b909600988a8e0e0adcbf71826800430c3f
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2024-05-13 16:33:14 -0300

    Service patiente (#13)
    * Cria construtor
    
    * Cria modelo para a entidade Professional
    
    * Atualiza model
    
    * Altera model para melhor tratamento do projeto (#6)
    
    * Eric (#8)
    
    * Cria construtor
    
    * Classe Patient
    
    * Cria classe Patient
    
    * Criar classe Patient
    
    * Criar classe patient 3
    
    ---------
    
    Co-authored-by: Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
    
    * Model address (#10)
    
    * adicionando model address
    
    * alterando model address
    
    * Atualiza model
    
    * Altera model para melhor tratamento do projeto (#6)
    
    * Feature/create context provider (#12)
    
    * Create context of provider
    
    * Create dto of provider
    
    * Add referent the provider in professional
    
    * Add namespace
    
    * Cria contexto do provider
    
    * Create Patient service, interface, controller
    
    * Corrige mapeamento
    
    * Remove migrations.
    
    * Muda tipo da data.
    
    * Delete AppDbContextModelSnapshot.cs
    
    ---------
    
    Co-authored-by: arthur.paula <arthur.paula@autoglass.com.br>
    Co-authored-by: Arthur Augusto Paula Hardman <112453063+arthurhardman@users.noreply.github.com>
    Co-authored-by: Eric Andrade Eller <111186310+EricEller22@users.noreply.github.com>
    Co-authored-by: Pedro Henrique Ferreira da Silva <117300191+pedroohferreira@users.noreply.github.com>
    Co-authored-by: Eric Andrade Eller <ericaeeller34@gmail.com>

--- mais-consultas-api/Data/AppDbContext.cs
+++ mais-consultas-api/Data/AppDbContext.cs
@@ -5,23 +5,21 @@ namespace mais_consultas_api.Data
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Models.Provider> Providers { get; set; }
-        public DbSet<Professional> Professionals { get; set; }
-        public DbSet<Address> Address { get; set; }
-        public DbSet<Patient> Patient { get; set; }
-
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Professional>()
-                .HasOne(professional => professional.Provider)
-                .WithOne(provider => provider.Professional)
+            builder.Entity<Models.Provider>()
+                .HasOne(provider => provider.Professional)
+                .WithOne(professional => professional.Provider)
                 .HasForeignKey<Professional>(professional => professional.Id_Provider);
         }
+        
+        public DbSet<Models.Provider> Providers { get; set; }
+        public DbSet<Professional> Professionals { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Patient> Patient { get; set; }
     }
-
-    public DbSet<Professional> Professionals { get; set; }
 }
