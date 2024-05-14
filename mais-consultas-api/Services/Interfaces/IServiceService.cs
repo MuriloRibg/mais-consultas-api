cf1e33fd48fe2b32e10bcde486be38af08b22784
caiocassimiro <cr58303@gmail.com>
2024-05-13 22:22:02 -0300

    Add Service

---
+++ mais-consultas-api/Services/Interfaces/IServiceService.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mais_consultas_api.Models
+{
+    public class Service
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(45)]
+        public string Name { get; set; }
+
+        // Constructor
+        public Service()
+        {
+        }
+    }
+}
---
+++ mais-consultas-api/Services/ServiceService.cs
@@ -0,0 +1,75 @@
+﻿using mais_consultas_api.Data;
+using mais_consultas_api.Models;
+using mais_consultas_api.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mais_consultas_api.Services
+{
+    public class ServiceService : IServiceService
+    {
+        private readonly AppDbContext _context;
+
+        public ServiceService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Service Add(decimal price, string name)
+        {
+            Service newService = new Service
+            {
+                Price = price,
+                Name = name
+            };
+
+            _context.Services.Add(newService);
+            _context.SaveChanges();
+
+            return newService;
+        }
+
+        public IEnumerable<Service> GetAll()
+        {
+            return _context.Services.ToList();
+        }
+
+        public Service Get(int id)
+        {
+            return _context.Services.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            Service serviceToRemove = _context.Services.FirstOrDefault(y => y.Id == id);
+            if (serviceToRemove != null)
+            {
+                _context.Services.Remove(serviceToRemove);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Service not found");
+            }
+        }
+
+        public Service Update(int id, decimal price, string name)
+        {
+            Service serviceToUpdate = _context.Services.FirstOrDefault(y => y.Id == id);
+            if (serviceToUpdate != null)
+            {
+                serviceToUpdate.Price = price;
+                serviceToUpdate.Name = name;
+
+                _context.SaveChanges();
+
+                return serviceToUpdate;
+            }
+            else
+            {
+                throw new Exception("Service not found");
+            }
+        }
+    }
+}
